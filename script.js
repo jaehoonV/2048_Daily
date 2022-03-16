d75599ff0b8fdb1ff5ab456700b01e7b8f70d17b
jaehoonV <ljh1032112@gmail.com>
2022-03-16 23:34:59 +0900

    JavaScript - Game Logic 추가

--- script.js
+++ script.js
@@ -10,69 +10,130 @@ grid.randomEmptyCell().tile = new Tile(gameBoard)
 setupInput()
 
 function setupInput() {
-    window.addEventListener("keydown", handleInput, { once: true }) /* 키 입력시 handleInput함수를 한 번만 수행 */
+   window.addEventListener("keydown", handleInput, { once: true }) /* 키 입력시 handleInput함수를 한 번만 수행 */
 }
 
-function handleInput(e) {
-    console.log(e.key)
-    switch (e.key) {
-        case "ArrowUp":
-            moveUp()
-            break
-        case "ArrowDown":
-            moveDown()
-            break
-        case "ArrowLeft":
-            moveLeft()
-            break
-        case "ArrowRight":
-            moveRight()
-            break
-        default:
+async function handleInput(e) {
+   switch (e.key) {
+      case "ArrowUp":
+         if (!canMoveUp()) {
             setupInput()
             return
-    }
+         }
+         await moveUp()
+         break
+      case "ArrowDown":
+         if (!canMoveDown()) {
+            setupInput()
+            return
+         }
+         await moveDown()
+         break
+      case "ArrowLeft":
+         if (!canMoveLeft()) {
+            setupInput()
+            return
+         }
+         await moveLeft()
+         break
+      case "ArrowRight":
+         if (!canMoveRight()) {
+            setupInput()
+            return
+         }
+         await moveRight()
+         break
+      default:
+         setupInput()
+         return
+   }
 
-    // Other code
+   grid.cells.forEach(cell => cell.mergeTiles())
 
-    setupInput()
+   /* 모든 움직임에 새로운 랜덤 타일을 추가 */
+   const newTile = new Tile(gameBoard)
+   grid.randomEmptyCell().tile = newTile
+
+   /* 게임 종료 (위, 아래, 왼쪽, 오른쪽 모두 움직일 수 없을 경우) */
+   if (!canMoveUp() && !canMoveDown() && !canMoveLeft() && !canMoveRight()) {
+      newTile.waitForTransition(true).then(() => {
+         alert("You lose")
+      })
+      return
+   }
+
+   setupInput()
 }
 
 function moveUp() {
-    return slideTiles(grid.cellsByColumn)
+   return slideTiles(grid.cellsByColumn)
 }
 
 function moveDown() {
-    return slideTiles(grid.cellsByColumn.map(column => [...column].reverse())) /* moveUp과 반대, column을 reverse */
+   return slideTiles(grid.cellsByColumn.map(column => [...column].reverse())) /* moveUp과 반대, column을 reverse */
 }
 
 function moveLeft() {
-    return slideTiles(grid.cellsByRow)
+   return slideTiles(grid.cellsByRow)
 }
 
 function moveRight() {
-    return slideTiles(grid.cellsByRow.map(row => [...row].reverse()))
+   return slideTiles(grid.cellsByRow.map(row => [...row].reverse()))
 }
 
 function slideTiles(cells) {
-    cells.forEach(group => {
-        for (let i = 1; i < group.length; i++) {
+   return Promise.all(
+      cells.flatMap(group => {
+         const promises = []
+         for (let i = 1; i < group.length; i++) {
             const cell = group[i]
             if (cell.tile == null) continue
             let lastValidCell
             for (let j = i - 1; j >= 0; j--) {
-                const moveToCell = group[j]
-                if (!moveToCell.canAccept(cell.tile)) break
-                lastValidCell = moveToCell
+               const moveToCell = group[j]
+               if (!moveToCell.canAccept(cell.tile)) break
+               lastValidCell = moveToCell
             }
+
             if (lastValidCell != null) {
-                if (lastValidCell.tile != null) {
-                    lastValidCell.mergeTile = cell.tile
-                } else { /* 이미 타일이 없으면 현재 타일로 설정 */
-                    lastValidCell.tile = cell.tile
-                }
-                cell.tile = null /* 현재 있는 셀의 타일은 제거 */
+               promises.push(cell.tile.waitForTransition())
+               if (lastValidCell.tile != null) {
+                  lastValidCell.mergeTile = cell.tile
+               } else { /* 이미 타일이 없으면 현재 타일로 설정 */
+                  lastValidCell.tile = cell.tile
+               }
+               cell.tile = null
+               /* 현재 있는 셀의 타일은 제거 */
             }
-        }
-    });
+         }
+         return promises
+      })
+   )
+}
+
+function canMoveUp() {
+   return canMove(grid.cellsByColumn)
+}
+
+function canMoveDown() {
+   return canMove(grid.cellsByColumn.map(column => [...column].reverse()))
+}
+
+function canMoveLeft() {
+   return canMove(grid.cellsByRow)
+}
+
+function canMoveRight() {
+   return canMove(grid.cellsByRow.map(row => [...row].reverse()))
+}
+
+function canMove(cells) {
+   return cells.some(group => {
+      return group.some((cell, index) => {
+         if (index === 0) return false
+         if (cell.tile == null) return false
+         const moveToCell = group[index - 1] /* 다음으로 움직여질 셀 */
+         return moveToCell.canAccept(cell.tile)
+      })
+   })
 }
